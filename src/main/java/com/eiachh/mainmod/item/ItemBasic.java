8c3b5e2c12abc6bf2ebe961ad77e79e1f152ae99
eiachh <srankoadam@gmail.com>
2018-12-24 23:22:27 +0100

    first upload
    firstUpload

---
+++ src/main/java/com/eiachh/mainmod/item/ItemBasic.java
@@ -0,0 +1,16 @@
+package com.eiachh.mainmod.item;
+
+import com.eiachh.mainmod.MainModpls;
+
+import net.minecraft.item.Item;
+ 
+public class ItemBasic extends Item {
+ 
+    public ItemBasic(String unlocalized,String Name) 
+    {
+        setUnlocalizedName(MainModpls.MODID+"."+ unlocalized);
+        setRegistryName(Name);
+        setCreativeTab(MainModpls.TUTORIAL_TAB);
+    }
+ 
+}
